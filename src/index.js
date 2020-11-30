8300040f3734df040505b7f40e192ea79aa99287
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-11-29 20:43:42 -0800

    2 instances using props

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'
 
-
 function Booklist() {
   return (
     <section className="booklist">
       <Book title='If Animals Kissed Good Night' author='Ann WhitFord Paul' img='https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg' />
-      <Book title='Name of Book' author='Name of Author' img='' />
-      <Book title='' author='' img='' />
+      <Book title='Humans' author='Brandon Stanton' img='https://m.media-amazon.com/images/I/81GeAcdMCsL._AC_UY218_.jpg' />
       <Book title='' author='' img='' />
       <Book title='' author='' img='' />
       <Book title='' author='' img='' />
