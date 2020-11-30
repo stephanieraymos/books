9f3e88d0012aafccd51d82b4391b26a67a0ac404
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-11-29 20:28:46 -0800

    Moved variables up + added variable for img

--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'
 
+
+//Variables
+const title = 'If Animals Kissed Good Night'
+const author = 'Ann WhitFord Paul'
+const img = 'https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg'
+
+
 function Booklist() {
   return (
     <section className="booklist">
       <Book />
     </section>
-  )
+  );
 }
 
 const Book = () => {
-  const title = 'If Animals Kissed Good Night'
-  const author = 'Ann WhitFord Paul'
   return (
     <article className="book">
-      <img src="https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg" alt="" />
+      <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>By: {author}</h4>
+      <p></p>
     </article>
   );
 };
