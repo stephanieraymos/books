8ff03c576a805b2dcb5cc1346ee1294114f05fbc
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-11-29 20:18:57 -0800

    Author stying

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const Book = () => {
     <article className="book">
       <img src="https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg" alt="" />
       <h1>If Animals Kissed Good Night</h1>
-      <h1>By: Ann WhitFord Paul</h1>
+      <h4>By: Ann WhitFord Paul</h4>
     </article>
   );
 };
