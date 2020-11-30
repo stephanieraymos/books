4f12a98c292f9addea6a93630831eaa802c115af
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-11-29 20:15:34 -0800

    Clean up

--- src/index.js
+++ src/index.js
@@ -6,40 +6,17 @@ function Booklist() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   )
 }
 
 const Book = () => {
   return <article className="book">
-    <Title />
-    <Author />
-    <Image />
+    <img src="https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg" alt="" />
+    <h1>If Animals Kissed Good Night</h1>
+    <h1>By: Ann WhitFord Paul</h1>
   </article>
 }
-
-const Image = () => {
-  return <img src="https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg" alt="" />
-}
-
-const Title = () => <h1>If Animals Kissed Good Night</h1>
-const Author = () => <h1 style={{ color: '#617d98', fontSize: '0.75rem', margin: '0.25rem' }}>By: Ann WhitFord Paul</h1>
-
-
-
 
 
 
