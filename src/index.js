f67e91e9f69e113be24c7566c48ca5f18ecf6f67
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-12-27 11:06:08 -0800

    Book edits

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 
 const books = [
   { id: 1,
-    title: "If Animals Kissed Good Night",
-    author: "Ann WhitFord Paul",
+    title: "Waking Up",
+    author: "Sam Harris",
     img:
-      "https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg",
+      "https://images-na.ssl-images-amazon.com/images/I/61POkmGjDqL.jpg",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "For the millions of Americans who want spirituality without religion, Sam Harris’s latest New York Times bestseller is a guide to meditation as a rational practice informed by neuroscience and psychology.",
   },
   { id: 2,
     title: "Human",
