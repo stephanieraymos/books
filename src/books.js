02ff9ad3f5403e56f8b02716c66fe8c0a6c4c0af
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-01 20:02:36 -0800

    Human desc

--- src/books.js
+++ src/books.js
@@ -21,6 +21,6 @@ export const books = [
       author: "Brandon Stanton",
       img: "https://m.media-amazon.com/images/I/81GeAcdMCsL._AC_UY218_.jpg",
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "This book showcases so many seemingly ordinary lives and shows us how we are all so unique. The Author uses his amazing photography skills to capture breathtaking images, along side a brief summary of each person's life.",
     },
   ];
